ae15a5121397589339efde5b6052c23021ad8ee4
vierukundo <vierukundo@gmail.com>
2023-10-05 14:52:12 -0700

    some improvement

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -30,20 +30,26 @@ def do_deploy(archive_path):
         run("mkdir -p /data/web_static/releases/{}/".format(archive_no_ext))
 
         # Uncompress the archive to /data/web_static/releases/<archive_no_ext>/
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(archive_filename, archive_no_ext))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
+            archive_filename, archive_no_ext))
 
         # Delete the archive from the server
         run("rm /tmp/{}".format(archive_filename))
 
         # Move the uncompressed files to the final destination
-        # run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(archive_no_ext, archive_no_ext))
+        # run("mv /data/web_static/releases/{}/web_static/*
+        # /data/web_static/releases/{}/".format(
+        # archive_no_ext, archive_no_ext))
 
-        # run("rm -rf /data/web_static/releases/{}/web_static".format(archive_no_ext))
+        # run("rm -rf /data/web_static/releases/{}/web_static".format(
+        # archive_no_ext))
         # Delete the symbolic link /data/web_static/current
         run("rm -rf /data/web_static/current")
 
         # Create a new symbolic link
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(archive_no_ext))
+        run(
+                "ln -s /data/web_static/releases/{}/ "
+                "/data/web_static/current".format(archive_no_ext))
 
         return True
 
