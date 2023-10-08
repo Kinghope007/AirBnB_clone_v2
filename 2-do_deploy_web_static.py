a1318136f0e937856b2496846874381d0f60a809
vierukundo <vierukundo@gmail.com>
2023-10-08 20:54:14 +0000

    modified with more descriptive variable names

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -34,38 +34,33 @@ def do_deploy(archive_path):
         return False
 
     try:
-        archive_filename = archive_path.split('/')[-1]
-        archive_no_ext = archive_filename.split('.')[0]
+        archive_with_ext = archive_path.split('/')[-1]
+        archive_no_ext = archive_with_ext.split('.')[0]
 
         # Upload the archive to /tmp/
         put(archive_path, "/tmp/")
 
         # Create the folder /data/web_static/releases/<archive_no_ext>/
         run("mkdir -p /data/web_static/releases/{}/".format(archive_no_ext))
-
+      
+        dest_path = "/data/web_static/releases/{}/".format(archive_no_ext)
         # Uncompress the archive to /data/web_static/releases/<archive_no_ext>/
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
-            archive_filename, archive_no_ext))
+        run("tar -xzf /tmp/{} -C {}".format(archive_with_ext, dest_path))
 
         # Delete the archive from the server
-        run("rm /tmp/{}".format(archive_filename))
+        run("rm /tmp/{}".format(archive_with_ext))
 
         # Move the uncompressed files to the final destination
-        run(
-                "mv /data/web_static/releases/{}/web_static/* /data/"
-                "web_static/releases/{}/".format(
-                    archive_no_ext, archive_no_ext))
+        run("mv {}web_static/* {}".format(dest_path, dest_path))
 
-        run("rm -rf /data/web_static/releases/{}/web_static".format(
-            archive_no_ext))
+        run("rm -rf {}web_static".format(dest_path))
 
         # Delete the symbolic link /data/web_static/current
         run("rm -rf /data/web_static/current")
 
         # Create a new symbolic link
         run(
-                "ln -s /data/web_static/releases/{}/ /data/web_static/"
-                "current".format(archive_no_ext))
+                "ln -s {} /data/web_static/current".format(dest_path))
 
         return True
 
