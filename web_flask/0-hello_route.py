7af947c9d403d6a5a775f7cd02fb351178cc3453
vierukundo <vierukundo@gmail.com>
2023-10-19 19:27:31 +0000

    script that starts a Flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Importing lightweight WSGI web application framework"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+# Define a route with the option strict_slashes=False
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
