8c5b280ea2e7c4efcac3161c497d8e14171caca9
vierukundo <vierukundo@gmail.com>
2023-10-20 05:43:54 -0700

    Corrected code to pass pycodestyle

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -5,20 +5,24 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
+
 # Define a route with the option strict_slashes=False
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def display_c_text(text):
     # Replace underscores with spaces
     text = text.replace('_', ' ')
     return 'C {}'.format(escape(text))
+
 
 # Specify the default value for the 'text' parameter in the route
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
@@ -27,6 +31,7 @@ def display_python_text(text):
     # Replace underscores with spaces
     text = text.replace('_', ' ')
     return 'Python {}'.format(escape(text))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -5,20 +5,24 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
+
 # Define a route with the option strict_slashes=False
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def display_c_text(text):
     # Replace underscores with spaces
     text = text.replace('_', ' ')
     return 'C {}'.format(escape(text))
+
 
 # Specify the default value for the 'text' parameter in the route
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
@@ -28,9 +32,11 @@ def display_python_text(text):
     text = text.replace('_', ' ')
     return 'Python {}'.format(escape(text))
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def display_number(n):
     return "{} is a number".format(escape(n))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
