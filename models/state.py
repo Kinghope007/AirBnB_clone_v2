ea20cc8540ed69f0f11eec478197202a87741668
vierukundo <vierukundo@gmail.com>
2023-10-20 21:08:45 +0000

    update

--- models/state.py
+++ models/state.py
@@ -4,21 +4,27 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from os import getenv
+
+storage_engine = getenv("HBNB_TYPE_STORAGE")
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="delete")
+    if storage_engine == "db":
+	name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade="delete")
+    else:
+	name = ""
 
-    @property
-    def cities(self):
-        """returns the list of City instances with
-        state_id equals to the current State.id"""
-        matching_cities = []
-        city_instances = storage.all().values()
-        for city in city_instances:
-            if city.state_id == self.id:
-                matching_cities.append(city)
-        return (matching_cities)
+        @property
+        def cities(self):
+            """returns the list of City instances with
+            state_id equals to the current State.id"""
+            matching_cities = []
+            city_instances = storage.all().values()
+            for city in city_instances:
+                if city.state_id == self.id:
+                    matching_cities.append(city)
+            return (matching_cities)
