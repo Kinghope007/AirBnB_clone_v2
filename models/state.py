f4bba1316ea81119aff5a291fdc2e645a9dae823
vierukundo <vierukundo@gmail.com>
2023-10-21 12:26:14 -0700

    Added root to display list of states

--- models/state.py
+++ models/state.py
@@ -14,7 +14,7 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="delete")
-    
+
     if storage_engine != "db":
         name = ""
 
